672b14516b570d10341df0d0e48886cd3eb09277
olga <olga.goryszewska@gmail.com>
2024-08-22 09:07:21 +0200

    adding home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from "react";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <main>
+      <Home />
+    </main>
+  )
 }
+
+export default App;
