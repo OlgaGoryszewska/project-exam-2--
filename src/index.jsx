7ff66a56ae1888978dfc2f33dc8106e63850c0e2
olga <olga.goryszewska@gmail.com>
2024-08-22 17:42:53 +0200

    adding and installing react DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {Route, BrowserRouter as Router} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Router>
     <App />
-  </StrictMode>,
-);
+  </Router>
+  )
