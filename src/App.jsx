436f98724c071ed6ababc685e440f8748d078a4f
olga <olga.goryszewska@gmail.com>
2024-08-22 19:56:41 +0200

    adding nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom"; 
+import Nav from "./components/Nav";
+
+// Pages
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <main>
-      <Home />
-      <Login />
-    </main>
-  )
+
+    <div>
+      <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-import {Route, BrowserRouter as Router} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./index.css";
 
@@ -11,4 +11,4 @@ root.render(
   <Router>
     <App />
   </Router>
-  )
+  );
