96fcaafe6277e0d361c0839ca1b426a888029223
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2024-08-23 13:59:50 +0200

    adding a dropdown many to app

--- src/components/DropDownMenu.jsx
+++ src/components/DropDownMenu.jsx
@@ -1,1 +1,25 @@
 import {useState} from 'react';
+import { Link } from "react-router-dom";    
+
+function DropDownMenu() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowMenu(!showMenu)}>Menu</button>
+      {showMenu && (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
+  );
+}
+export default DropDownMenu;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
 
 import logo from '../assets/img/logo.png';
+import DropDownMenu from "./DropDownMenu";
 
 function Nav() {
   return (
     <>
       <header className="flex flex-row bg-deep-blue h-12">
         <img className=" h-11 "src={logo} alt="Logo for a nav bar" />
-        <nav >
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+        <DropDownMenu/>
       </header>
     </>
   );
