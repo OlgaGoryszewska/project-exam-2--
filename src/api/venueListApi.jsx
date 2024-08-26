2aa479a8be94072d052fe85cfe0969f7029c1081
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2024-08-26 10:52:13 +0200

    adding fetching Venues

--- src/api/venueListApi.jsx
+++ src/api/venueListApi.jsx
@@ -1,3 +1,51 @@
 //import { API_BASE_URL } from "./config";
+import { useEffect, useState } from "react";
 
+
+function VenueListApi() {
+  const [venueList, setVenueList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchVenueList() {
+      try {
+        const response = await fetch("https://v2.api.noroff.dev/api/venues");
+        const data = await response.json();
+        setVenueList(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchVenueList();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Something went wrong: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Venue List</h1>
+      <ul>
+        {venueList.map((venue) => (
+          <li key={venue.id}>
+            <h2>{venue.name}</h2>
+            <p>{venue.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+
+
+}
+
+export default VenueListApi;
 
