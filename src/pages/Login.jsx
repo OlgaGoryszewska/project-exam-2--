121ce9d2867be94bf64b58bec6832fdc28191317
olga <olga.goryszewska@gmail.com>
2024-09-12 07:38:34 +0200

    exports

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
-
+import LoginForm from "../components/LoginForm" 
 
 function Login() {
     return (
         <div>
             <h1>Login</h1>
+            <LoginForm />
 
         </div>
     )
