aa9d7e50161d862c1a3c223ff1572773c4b1fd17
olga <olga.goryszewska@gmail.com>
2024-09-29 08:20:50 +0200

    adding animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,18 @@
 import GetVenueList from '../services/VenueList'
-import heroImg from '../assets/img/heroImg.png'
 import SearchVenue from '../components/SearchVenue'
 import Nav from '../components/Nav'
 import { useAuthState } from '../hooks/useAuthState'
-import { useEffect } from 'react'
-import { gsap } from 'gsap'
-import { useRef } from 'react'
+import { HeroImg } from '../components/HeroImg'
 
 function Home() {
     const { profile } = useAuthState()
-    const imageRef = useRef(null)
-
-    useEffect(() => {
-        gsap.from(imageRef.current, {
-            duration: 1,
-            scale: 1,
-            opacity: 0,
-            ease: 'back.out(1.7)',
-            delay: 0.5,
-            transformOrigin: 'center',
-        })
-    }, [])
 
     return (
         <>
             <Nav profile={profile} />
             <div className="flex flex-col bg-white">
-                <img
-                    className="max-h-800  object-cover "
-                    src={heroImg}
-                    alt="home"
-                    ref={imageRef}
-                />
+                <HeroImg />
+
                 <div className="flex flex-col max-w-4xl mx-auto ">
                     <h1 className="text-center p-4 ">
                         Discover the best retreats of Sri Lanka
